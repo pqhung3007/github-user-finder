213aef3a87eda82dd2e1de5506c8a2401f37909e
Windows 10 <hungpham307@gmail.com>
2022-01-10 14:31:05 +0700

    fix daisy-ui malfunctioning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
-    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
   },
   plugins: [require('daisyui')],
 }
